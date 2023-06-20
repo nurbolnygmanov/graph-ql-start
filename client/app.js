184950a3f0fbaf82db28bf8626ea0d054422b490
nurbol <nurbolnygmanov@gmail.com>
2023-06-20 09:14:36 +0200

    starter

---
+++ client/app.js
@@ -0,0 +1,18 @@
+async function fetchGreeting() {
+  const res = await fetch("http://localhost:9000/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: "query {greeting}",
+    }),
+  });
+
+  const { data } = await res.json();
+  return data.greeting;
+}
+
+fetchGreeting().then((greeting) => {
+  document.getElementById("greeting").textContent = greeting;
+});
